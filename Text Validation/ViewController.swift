8d10a5175111b39c5f9553c89c0de747c7a8d29a
Jane Madsen <janemadsen815@gmail.com>
2023-02-23 09:38:33 -0700

    Complete simple text validation app

--- Text Validation/ViewController.swift
+++ Text Validation/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Error {
+    case tooShort, needSpecial, empty, none
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var errorView: UIView!
+    @IBOutlet var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func validatePassword() -> Error {
+        guard let input = passwordTextField.text else {return .empty}
+        
+        let range = NSRange(location: 0, length: input.utf16.count)
+        
+        let pwRegex = try! NSRegularExpression(pattern: "[@,.!@#$%^&*<>?+=-]")
+        
+        if input.count < 8 {
+            return .tooShort
+        } else if pwRegex.firstMatch(in: input, options: [], range: range) != nil {
+            return .none
+        } else {
+            return .needSpecial
+        }
+    }
+    
+    fileprivate func showError(_ label: String) {
+        errorView.isHidden = false
+        errorLabel.text = label
+    }
+    
+    @IBAction func submitLogin(_ sender: Any) {
+        let passwordValidatedError = validatePassword()
+        switch passwordValidatedError {
+        case .none:
+            performSegue(withIdentifier: "PasswordSuccess", sender: sender)
+        case .needSpecial:
+            showError("Please include at least one special character: \"@,.!@#$%^&*<>?+=-\"")
+        case .tooShort:
+            showError("Please use at least 8 characters in your password.")
+        case .empty:
+            showError("Please enter a password.")
+        }
+    }
+    
+    
+    @IBAction func dismissErrorView(_ sender: Any) {
+        errorView.isHidden = true
+    }
+    
+    
 }
+
+
 
